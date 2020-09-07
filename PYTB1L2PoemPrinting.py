d0065f29c52e060aef5351dabefcd62d91a9fcf6
AidanMiddel <aidanm@ctjies.nl>
2020-09-07 11:43:44 +0200

    7 sept

---
+++ PYTB1L2PoemPrinting.py
@@ -0,0 +1,14 @@
+vers1 = "Er was eens een jongen genaamd Aidan,"
+vers2 = "Hij kwam helemaal uit Haarlem zodat hij straks programmeren kan."
+vers3 = "In zijn vrije tijd is hij wel eens met computers bezig."
+vers4 = "Maar gewoon om wat dingetjes te proberen en dus nog niet programmeren"
+vers5 = "zelf doet hij ook veel aan gamen in zijn vrijtijd, geen verrassing geheid."
+vers6 = "eigenlijk is dit programmeren redelijk simpel, dus hoop ik over 4 jaar geslaagd te zijn met vlag en wimpel"
+
+
+print(vers1)
+print(vers2)
+print(vers3)
+print(vers4)
+print(vers5)
+print(vers6)
---
+++ test.py
@@ -0,0 +1,8 @@
+print("Hello world!")
+
+naam = input()
+print("hi my comrade, " + naam)
+print("nice to meet you!")
+print("boem!!")
+
+input("press any key to continue (shit that sounded professional)")
