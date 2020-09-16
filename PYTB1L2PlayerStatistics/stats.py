9857240e84042257424ea3f522f38cb022585f17
AidanMiddel <aidanm@ctjies.nl>
2020-09-16 12:19:01 +0200

    player stats

--- PYTB1L2PlayerStatistics/stats.py
+++ PYTB1L2PlayerStatistics/stats.py
@@ -1,3 +1,31 @@
 name = "Aidan"
-weight = "meduim"
-length = "1,80"
+weight = "medium"
+length = "average"
+doublejump = True
+charactertype = "defender"
+accuracy = "[██████-----]"
+speed = "       [████████---]"
+sneek = "       [███--------]"
+healt = "      [██████-----]"
+
+print("name: " , name)
+print("size: " , weight)
+print("class: " , charactertype)
+print("ability to doublejump: " , doublejump)
+print ("health: " , healt)
+print("gun accuracy: " , accuracy)
+print("speed: " , speed)
+print("sneak: " , sneek)
+print(" ")
+kiezen = input("press A to choose or B to denie:")
+if kiezen == "A":
+    print(" ")
+    print("lets go")
+    print(" ")
+    print("couldn't find a game...")
+elif kiezen == "B":
+    print(" ")
+    print("failed to load more characters for the game :(")
+else:
+    print(" ")
+    print("or not")
