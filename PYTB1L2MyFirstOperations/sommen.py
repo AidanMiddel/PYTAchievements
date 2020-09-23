88fe59597032274f65d8b72159eaa1ea0a86cc54
AidanMiddel <aidanm@ctjies.nl>
2020-09-23 11:58:15 +0200

    sommen

--- PYTB1L2MyFirstOperations/sommen.py
+++ PYTB1L2MyFirstOperations/sommen.py
@@ -5,23 +5,21 @@ print("reken opdracht python")
 time.sleep
 print("")
 print("1 + 1 = ",1 + 1  ,) #berekent met +
-time.sleep(2)
 print()
 print("8 - 6 = ",8 - 6 ) #berekent met -
-time.sleep(2)
 print()
 print ("3 x 32 = ",3 * 32 ) #berekent met x
-time.sleep(2)
 print()
 print("9 : 3 = ",9 / 3) #berekent met /
-time.sleep(2)
 print()
 print("7 : 2 = ",7 % 2) #berekent met %
-time.sleep(2)
-a = 10
+a = 5
+b = 9
+a += b
 print()
-print("a = 10,   a + 5 = ", a + 5) #berekent met +
-time.sleep(2)
+print("a = 5,   b = 9,   a + b = ", a) #berekenen met variabele +
+a = 64
+b = 8
+a /= b
 print()
-print("a = 15,   a / 5 = ", a  / 5) #berekent met /
-time.sleep(2)
+print("a = 64,  b = 8,   a / b = " , a)
