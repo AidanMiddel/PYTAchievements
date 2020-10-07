77cd5c6f258e9090da0ccb427eb46e1eda0dcd2b
AidanMiddel <aidanm@ctjies.nl>
2020-10-07 12:05:21 +0200

    keuzes

---
+++ PYTB1L3DailyChoices/keuze.py
@@ -0,0 +1,41 @@
+#oké hier gaan we
+import time
+print("Hallo welkom bij: HET DAGELIJKS LEVEN....")
+print("")
+time.sleep(1)
+print("klinkt eng of niet zeker?")
+print("")
+time.sleep(1)
+print("laten we maar beginnen...")
+print("")
+time.sleep(1)
+print("")
+print("je ligt lekker in bed te slapen tot je ineens een geluid hoort \neehhh eeehhh eehhhh eehh eehhh \nhet is je wekker....")
+print("wat doe je?")
+print("A. zet hem op snooze en wordt de volgende keer wakker \nB. ga je bed uit" )
+print("")
+wakkerworden = input("A of B(let op hoofdletters): ")
+if wakkerworden == "A":
+    print("")
+    print("ah jij blijft nog lekker even liggen zeker....")
+elif wakkerworden == "B":
+    print("")
+    print("ochtend mens? of gewoon zin in de dag?")
+else:
+    print("")
+    print("let op hoofdletters! maar ik ga wel verder")
+print("")
+time.sleep(1)
+print("dus je bent je bed uit \nnou is de vraag, ga je douchen?")
+print("A. nee niet nu \nB. ja ik ga nu even douchen")
+print("")
+douchen = input("A of B(let op hoofdletters): ")
+if douchen == "A":
+    print("")
+    print("oh oké, zolang je het maar vanavond doet")
+elif douchen == "B":
+    print("")
+    print("lekker even in de ochtend douchen")
+else:
+    print("")
+    print("let op hoofdletters! maar ik ga wel verder")
